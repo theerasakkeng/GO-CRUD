a24058d91fd69309c37c2bfbf7a0fa98239633cb
Theerasak Phansai <theerasak_fusrodah@hotmail.com>
2023-05-04 05:18:53 +0700

    [Add] post

--- models/customer_models.go
+++ models/customer_models.go
@@ -11,3 +11,18 @@ type Customer_Response struct {
 	State       string  `json:"state"`
 	Zip_Code    string  `json:"zip_code"`
 }
+
+type Customer_Req struct {
+	First_Name string  `json:"first_name"`
+	Last_Name  string  `json:"last_name"`
+	Phone      *string `json:"phone"`
+	Email      string  `json:"email"`
+	Street     string  `json:"street"`
+	City       string  `json:"city"`
+	State      string  `json:"state"`
+	Zip_Code   string  `json:"zip_code"`
+}
+
+func (Customer_Req) TableName() string {
+	return "sales.customers"
+}
